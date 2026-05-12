81fcfa02b2b9997b35857b10767c28809d8411c5
aialmohamed <ahmaedibrahim311@gmail.com>
2026-05-12 17:35:37 +0200

    Hinzugefügt in Setup.py

--- src/orange-ros2/kr_example/python/setup.py
+++ src/orange-ros2/kr_example/python/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            
+            'control_by_controller = my_package.control_by_controller:main'
         ],
     },
 )
